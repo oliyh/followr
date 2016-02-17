a68905a4af3704b25bf6cb21a250ebb3513654b6
Oliver Hine <git@oliy.co.uk>
2016-02-17 22:09:40 +0000

    Basic integration implementations

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [jarohen/nomad "0.7.2"]
                  [clj-http "2.0.1"]
-                 [com.flickr4java/flickr4java "2.15"]]
+                 [clj-oauth "1.5.4"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/followr/flickr.clj
+++ src/followr/flickr.clj
@@ -1,26 +1,44 @@
 (ns followr.flickr
-  (:require [followr.config :refer [config]])
-  (:import [com.flickr4java.flickr Flickr  REST]
-           [com.flickr4java.flickr.auth Permission]
-           [org.scribe.model Token Verifier]))
+  (:require [followr.config :refer [config]]
+            [clj-http.client :as http]
+            [oauth.client :as oauth]))
 
-(defn- flickr-auth []
-  (let [{:keys [flickr-key flickr-secret]} (config)
-        flickr (Flickr. flickr-key flickr-secret (REST.))]
-    (.getAuthInterface flickr)))
+(def flickr-rest-endpoint "https://api.flickr.com/services/rest")
 
-(defn request-auth []
-  (let [flickr-auth (flickr-auth)
-        request-token (.getRequestToken flickr-auth)]
-    (println (format "Visit %s to obtain authorisation code"
-                     (.getAuthorizationUrl flickr-auth request-token Permission/DELETE)))
+(defonce consumer (delay
+                   (let [{:keys [consumer-key consumer-secret]} (config)]
+                     (oauth/make-consumer consumer-key
+                                          consumer-secret
+                                          "https://www.flickr.com/services/oauth/request_token"
+                                          "https://www.flickr.com/services/oauth/access_token"
+                                          "https://www.flickr.com/services/oauth/authorize"
+                                          :hmac-sha1))))
 
-    (fn obtain-access-token [auth-code]
-      (let [access-token (.getAccessToken flickr-auth request-token (Verifier. auth-code))
-            auth (.checkToken flickr-auth access-token)]
-        {:request-token (.getToken access-token)
-         :request-secret (.getSecret access-token)
-         :nsid (.. auth getUser getId)
-         :real-name (.. auth getUser getRealName)
-         :user-name (.. auth getUser getUsername)
-         :permission (.. auth getPermission getType)}))))
+;; doesn't work
+#_(def request-token (oauth/request-token consumer "http://localhost/oauth"))
+#_(oauth/user-approval-uri consumer (:oauth_token request-token))
+
+(defn- credentials [op]
+  (let [{:keys [token token-secret]} (config)]
+    (oauth/credentials @consumer
+                       token
+                       token-secret
+                       :POST
+                       flickr-rest-endpoint
+                       op)))
+
+(defn call-flickr [op]
+  (let [op (merge {:format "json"
+                   :nojsoncallback 1} op)]
+    (http/post flickr-rest-endpoint {:query-params (merge (credentials op) op)
+                                     :as :json})))
+
+
+(defn add-contact [user-id]
+  (call-flickr {:method "flickr.contacts.add"
+                :user_id user-id
+                :family 0
+                :friend 0}))
+
+(defn list-contacts []
+  (call-flickr {:method "flickr.contacts.getList"}))
